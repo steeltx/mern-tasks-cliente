f2af4d46ba67c543211069ae6bec66a621278e98
steeltx <realsteel.gnu@gmail.com>
2021-02-14 01:01:51 -0600

    Validar formulario de obtener cuenta y mostrar alerta

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,11 +1,16 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AlertaContext from '../../context/alertas/alertaContext';
 
 const NuevaCuenta = () => {
 
+    // extraer los valores del context
+    const alertaContext = useContext(AlertaContext);
+    const { alerta, mostrarAlerta } = alertaContext;
+
     // state para iniciar sesion
     const [usuario, guardarUsuario] = useState({
-        nombre:'',
+        nombre: '',
         email: '',
         password: '',
         confirmar: ''
@@ -18,7 +23,7 @@ const NuevaCuenta = () => {
     const onChange = e => {
         guardarUsuario({
             ...usuario,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         });
     }
 
@@ -27,17 +32,30 @@ const NuevaCuenta = () => {
         e.preventDefault();
 
         // validar que no haya campos vacios
+        if (nombre.trim() === '' || email.trim() === '' || password.trim() === '' || confirmar.trim() === '') {
+            mostrarAlerta('Todos los campos son obligatorios', 'alerta-error');
+            return;
+        }
 
         // password minimo de 6 caracteres
+        if (password.length < 6) {
+            mostrarAlerta('El password debe ser al menos de 6 caracteres', 'alerta-error');
+            return;
+        }
 
         // los 2 passwords son iguales
+        if(password !== confirmar){
+            mostrarAlerta('Los passwords no son iguales', 'alerta-error');
+            return;
+        }
 
         // pasarlo al action
-
+        
     }
 
     return (
         <div className="form-usuario">
+            { alerta ? (<div className={`alerta ${alerta.categoria}`}>{alerta.msg}</div>) : null}
             <div className="contenedor-form sombra-dark">
                 <h1>Obtener una cuenta</h1>
                 <form
